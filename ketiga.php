a7dd13c33d9ff97857d2c772a03489acf97e9350
OniSakadev <zulfaishalilham@gmail.com>
2024-09-03 22:55:37 +0700

    Praktikum 4B - Membuat Atribut

--- ketiga.php
+++ ketiga.php
@@ -1,13 +1,31 @@
 <?php
-//Gabungan antara praktikum 4B dan 4C
-class Lingkaran
+
+class Kerucut
 {
     const PHI = 3.14;
     public $jari_jari;
 
-    public function __construct($jari_jari) {
-        $this->jari_jari = $jari_jari;
+    public function volume($tinggi) : float {
+        return (1/3) * self::PHI * pow($this->jari_jari, 2) * $tinggi;
     }
+}
+
+// Membuat objek dari kelas Kerucut
+$nasi_tumpeng = new Kerucut();
+
+// Mengatur nilai jari-jari secara langsung
+$nasi_tumpeng->jari_jari = 4;
+
+// Menghitung volume dari nasi_tumpeng dengan tinggi 10 cm
+$volume_nasi_tumpeng = $nasi_tumpeng->volume(10);
+
+// Menampilkan hasilnya
+echo "Volume nasi tumpeng adalah: {$volume_nasi_tumpeng} \n";
+
+class Lingkaran
+{
+    const PHI = 3.14;
+    public $jari_jari;
 
     public function luas() : float {
         return self::PHI * $this->jari_jari * $this->jari_jari;
@@ -18,14 +36,24 @@ class Lingkaran
     }
 }
 
+// Membuat objek dari kelas Lingkaran
+$lingkaran = new Lingkaran();
+
+// Mengatur nilai jari-jari secara langsung
+$lingkaran->jari_jari = 7;
+
+// Menghitung luas dan keliling lingkaran
+$luas_lingkaran = $lingkaran->luas();
+$keliling_lingkaran = $lingkaran->keliling();
+
+// Menampilkan hasilnya
+echo "Luas lingkaran adalah: {$luas_lingkaran} \n";
+echo "Keliling lingkaran adalah: {$keliling_lingkaran}";
+
 class Bola
 {
     const PHI = 3.14;
     public $jari_jari;
-
-    public function __construct($jari_jari) {
-        $this->jari_jari = $jari_jari;
-    }
 
     public function volume() : float {
         return (4/3) * self::PHI * pow($this->jari_jari, 3);
@@ -36,47 +64,16 @@ class Bola
     }
 }
 
+// Membuat objek dari kelas Bola
+$bola = new Bola();
 
-class Tabung
-{
-    const PHI = 3.14;
-    public function volume($jari_jari,$tinggi) : float {
-        return self::PHI*pow($jari_jari,2)*$tinggi;
-    }
-}
+// Mengatur nilai jari-jari secara langsung
+$bola->jari_jari = 5;
 
-
-class Kerucut
-{
-    const PHI = 3.14;
-    public $jari_jari;
-
-    public function __construct($jari_jari) {
-        $this->jari_jari = $jari_jari;
-    }
-
-    public function volume($tinggi) : float {
-        return (1/3)*self::PHI*pow($this->jari_jari,2)*$tinggi;
-    }
-}
-
-$lingkaran = new Lingkaran(7);
-
-$luas_lingkaran = $lingkaran->luas();
-$keliling_lingkaran = $lingkaran->keliling();
-
-echo "Luas lingkaran adalah: {$luas_lingkaran} \n";
-echo "Keliling lingkaran adalah: {$keliling_lingkaran} \n";
-
-$bola = new Bola(5);
-
+// Menghitung volume dan luas permukaan bola
 $volume_bola = $bola->volume();
 $luas_permukaan_bola = $bola->luas_permukaan();
 
+// Menampilkan hasilnya
 echo "Volume bola adalah: {$volume_bola} \n";
-echo "Luas permukaan bola adalah: {$luas_permukaan_bola} \n";
-
-$nasi_tumpeng = new kerucut(4);
-$volume_nasi_tumpeng = $nasi_tumpeng->volume(10);
-
-echo "Volume kerucut nasi tumpeng adalah: {$volume_nasi_tumpeng} \n ";
+echo "Luas permukaan bola adalah: {$luas_permukaan_bola}";
